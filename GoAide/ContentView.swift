db4b657a9eb967b527dea6f5bcbfa163a6386c82
Solidarité <d>
2026-06-17 17:05:44 +0200

    Initial Commit

---
+++ GoAide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoAide
+//
+//  Created by Dina TAOURDA on 17/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoAide/GoAideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoAideApp.swift
+//  GoAide
+//
+//  Created by Dina TAOURDA on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct GoAideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
